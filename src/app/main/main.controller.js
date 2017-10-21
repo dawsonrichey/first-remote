bf3a8855515837cb473c1b006283f99ea57e87f3
dawsonrichey <dawsonrichey89@gmail.com>
2017-10-21 01:27:55 -0400

    update

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,14 +1,14 @@
 export class MainController {
-  constructor () {
+  constructor ($http) {
     'ngInject';
 
-      
+      this.$http = $http;
       
   }
 
     
    postMessage() {
-       console.log("post")
+       this.$http.post('http://localhost:5000/api/message', {msg: 'hello'})
    } 
     
 }
